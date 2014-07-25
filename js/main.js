9d66b07e89bcf9b48517c33568ca8f4785122ae8
Febby <gunawan.febby@gmail.com>
2014-07-25 09:39:09 +0800

    added SCSC folder and updated main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,54 @@
 
+var local = $('.local-time');
+var result = $('.results');
+var url = "http://echo.jsontest.com/hello/world";
+
+//Get Current Time
+var getNow = function(){
+
+	//moment.js
+	var result = moment().format('YYYY-MM-DD | HH:MM:ss:SS');
+
+	return result;
+};
+
+//Displays current time in DOM
+var localClock = function (){
+	local.text(getNow());
+	//set Timeout 
+
+	t = setTimeout(function(){
+		localClock();},500);
+};
+
+//AJAX Call
+
+var asyncDemo = function(url){
+	$.ajax({
+		url: url ,
+		type: 'GET',
+		beforeSend: function(){
+			result.append('<p> Request sent at: ' + getNow() + '</p>');
+		}
+	})
+	.done(function(data) {
+		console.log("success");
+		result.append('<p> Response at: ' + getNow() + '</p>');
+		result.append('<p> Response object <em>hello</em> is: <b> ' + data.hello + '</b></p><hr>');
+	})
+	.fail(function() {
+		console.log("error");
+	})
+	// .always(function() {
+	// 	console.log("complete");
+	// });
+	
+};
+
+//Call asyncDemo and autoupdate localClock
+
+$(document).ready(function(){
+	localClock();
+	asyncDemo(url);
+
+});
